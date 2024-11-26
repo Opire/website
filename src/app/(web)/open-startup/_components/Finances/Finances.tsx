68b2eb2aa67a64b974165afc9da0df9cac2dea77
nabby27 <26057762+nabby27@users.noreply.github.com>
2024-11-26 20:20:34 +0100

    fix: copy

--- src/app/(web)/open-startup/_components/Finances/Finances.tsx
+++ src/app/(web)/open-startup/_components/Finances/Finances.tsx
@@ -9,7 +9,7 @@ export function Finances() {
                     We are a self-funded project. We don't have any external investors or funding at the moment. All the expenses are paid by the founders.
                 </p>
                 <p className={styles.description}>
-                    To demonstrate our commitment to Opire we invest around <strong>$600</strong> per month in bounties.
+                    To demonstrate our commitment to Opire, we invest a variable amount per month in bounties.
                 </p>
                 <p className={styles.description}>
                     Currently Opire is not generating any revenue. We are working on a revenue model to make this project sustainable in the long term.
