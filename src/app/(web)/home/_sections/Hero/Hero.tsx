ea7e66a30a68dc4f4527b66191824cf478825b10
nabby27 <26057762+nabby27@users.noreply.github.com>
2024-11-26 20:42:15 +0100

    fix: typo

--- src/app/(web)/home/_sections/Hero/Hero.tsx
+++ src/app/(web)/home/_sections/Hero/Hero.tsx
@@ -17,7 +17,7 @@ export function Hero() {
                 <div className={styles.content}>
                     <h1 className={styles.title}><span className={clsx(styles.title_not_highlight, styles.title_not_highlight_1)}>Fund</span> <br /><span className={styles.title_highlight}>Open Source</span> <br /><span className={clsx(styles.title_not_highlight, styles.title_not_highlight_2)}>issues</span></h1>
 
-                    <p className={styles.subtitle}>Anyone can create bounties in open-source projects and attract contributors, while developers can earn money ba solving issues</p>
+                    <p className={styles.subtitle}>Anyone can create bounties in open-source projects and attract contributors, while developers can earn money by solving issues</p>
 
                     <div className={styles.ctas}>
                         <OPButton size="big" href="https://app.opire.dev" target="_blank">GET STARTED NOW</OPButton>
